fc42d07fc345cb4c02d2605d6859058ae6b65709
Ingelheim <lukas.ingelheim@gmail.com>
2014-06-07 17:40:12 +0200

    added link to working client side socket.io library

--- public/core.js
+++ public/core.js
@@ -2,14 +2,9 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope, socket) {
 
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+  // on init: send username to socket
+  // 
+
 
   socket.on('init', function (data) {
     // $scope.name = data.name;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
 	app.use(express.static(__dirname + '/public'));
 
-  app.get('*', function(req, res) {
+  app.get('/', function(req, res) {
 		res.sendfile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 
