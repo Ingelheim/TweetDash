979b0e8e3145cec997c52280999cde571e7a0d1a
Ingelheim <lukas.ingelheim@gmail.com>
2014-06-07 16:41:49 +0200

    working skeleton

--- puplic/core.js
+++
@@ -1,5 +0,0 @@
-var scotchTodo = angular.module('scotchTodo', []);
-
-function mainController($scope, $http) {
-  $scope.test = {"hallo"}
-}
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 	var express  = require('express');
 	var app      = express();
 
+	app.use(express.static(__dirname + '/public'));
+
   app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 
 	app.listen(8000);
