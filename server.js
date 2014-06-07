ec81513bd7bc5684ec887e4879f3cf67b46b7882
Oliver Diestel <o.diestel@gmail.com>
2014-06-07 17:37:17 +0200

    added socket.io however doesnt work yet

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-	var express  = require('express');
-	var app      = express();
+var express  = require('express');
+var io       = require('socket.io');
+var app      = express();
 
-	app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-  app.get('*', function(req, res) {
-		res.sendfile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	});
+app.get('*', function(req, res) {
+	res.sendfile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
-	app.listen(8000);
-	console.log("App listening on port 8080");
+app.listen(8000);
+console.log("App listening on port 8080");
+
+io = io.listen(app, { log: false });
+
+var sockets = {};
+io.sockets.on('connection', function (socket) {
+  
+  var username = 'username'; //get user name from socket
+  if(sockets[username]) {
+    sockets[username].push(socket.id);
+  }
+  else {
+    sockets[username] = [socket.id];
+  }
+  socket.emit({'msg': {'user': 'TweetDash', 'msg': 'Welcome'}});
+});
+
+var sendUpdate = function() {
+  var updates = "{test}" //get twitter updates
+  if(updates.length) {
+    io.sockets.emit('message', "new Message");
+  }
+}
+
