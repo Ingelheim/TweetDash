281e7a8ffb322e0137cfb51d1f256cc540403527
Oliver Diestel <o.diestel@gmail.com>
2014-06-08 09:57:02 +0200

    jade partials

--- public/client.js
+++ public/client.js
@@ -18,8 +18,9 @@ tweetDashApp.controller('TweetDashCtrl', function ($scope, socket) {
 
   var functions = ['image', 'analytics'];  //in the future this array should be populated by the filnenames of public/hashtags/filename.html
   socket.on('msg', function(data){
-    var reg = /\#(\w*)/; //try to grab the second word after the hashtag for urls
+    var reg = /\#\w*/; //try to grab the second word after the hashtag for urls
     var hashtag = reg.exec(data['text']);
+    console.log(hashtag);
     if(hashtag && _.contains(functions, hashtag[0])) {
       $scope.templateUrl = hashtag[0].replace('#', '')+'.html'; //should use something like var reg = /\#(\w*)/
       console.log('templateUrl: '+$scope.templateUrl);
