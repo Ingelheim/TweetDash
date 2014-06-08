cb7300e608124b45835b2ee5c2baddbe6af8c0f3
Ulrike Schmidt <ulrike.schmidt@kanji-web.org>
2014-06-08 12:46:36 +0200

    extra config file

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ var app      = express();
 var http = require('http').Server(app);
 var io      = require('socket.io')(http);
 var twitter = require('twitter');
+var config = require('./config');
+
 var util = require('util');
-var twit = new twitter({
-	consumer_key: 'dHAtDthdr6ZG3OlOPQBu3cuRH',
-	consumer_secret: 'FNMM5iWEVS1yOOmB71OZiECJKd8h9ZeHZeDXLhlrkv0rJ0Kohw',
-	access_token_key: '998546844-O0CmoBh5ZeOwfxo1btcEY4lI5QyxKf7oQDd5kvFj',
-	access_token_secret: 'SMMovKIeOynju1BsaEuuHHCaC32zewlyVwLfuq4KwVd0j'
-});
+var twit = new twitter(config);
 
 
 app.use(express.static(__dirname + '/public'));
